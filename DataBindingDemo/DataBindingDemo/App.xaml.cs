e7436f5bd1005c11a2fb8ae0f7a9520e2689236a
KI-IAN <f.r.khan.trk@gmail.com>
2020-03-08 21:13:08 +0800

    ** View to view binding demo.

--- DataBindingDemo/DataBindingDemo/App.xaml.cs
+++ DataBindingDemo/DataBindingDemo/App.xaml.cs
@@ -12,7 +12,7 @@ namespace DataBindingDemo
             InitializeComponent();
 
             //Test Changes
-            MainPage = new Tutorial.RegularDataBindingDemo();
+            MainPage = new Tutorial.ViewToViewBindingDemo();
 
         }
 
