0e8149dc7e208bcdb30ec7fc0c5904730660ebc4
KI-IAN <f.r.khan.trk@gmail.com>
2020-03-08 21:48:18 +0800

    ** Ekka dokka demo.

---
+++ DataBindingDemo/DataBindingDemo/Tutorial/KitKitView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DataBindingDemo.Tutorial
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class KitKitView : ContentPage
+    {
+        public KitKitView()
+        {
+            InitializeComponent();
+        }
+    }
+}
