d23a5a3554c79657aa2a5575555c9c00ed6cb3d4
KI-IAN <f.r.khan.trk@gmail.com>
2020-03-11 22:00:47 +0800

    ** Listview demo using List of string.

---
+++ DataBindingDemo/DataBindingDemo/Tutorial/ListViewGenericListItem.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DataBindingDemo.Tutorial
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewGenericListItem : ContentPage
+    {
+        public ListViewGenericListItem()
+        {
+            InitializeComponent();
+
+
+            lvSampleName.ItemsSource = GetItem();
+
+
+        }
+
+
+        private List<string> GetItem()
+        {
+
+            List<string> items = new List<string>()
+            {
+                "Item#1",
+                "Item#2",
+                "Item#3",
+                "Item#4",
+                "Item#5",
+                                "Item#1",
+                "Item#2",
+                "Item#3",
+                "Item#4",
+                "Item#5",
+                                "Item#1",
+                "Item#2",
+                "Item#3",
+                "Item#4",
+                "Item#5",
+                                "Item#1",
+                "Item#2",
+                "Item#3",
+                "Item#4",
+                "Item#5",
+                                "Item#1",
+                "Item#2",
+                "Item#3",
+                "Item#4",
+                "Item#5",
+                                "Item#1",
+                "Item#2",
+                "Item#3",
+                "Item#4",
+                "Item#5",
+            };
+
+
+            return items;
+        }
+    }
+}
