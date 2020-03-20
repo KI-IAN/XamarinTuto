dc41ec833f1169529539c9269c29fb4898938131
KI-IAN <f.r.khan.trk@gmail.com>
2020-03-21 01:49:28 +0800

    ** Generating items in listview in MVVM way ;-)

---
+++ DataBindingDemo/DataBindingDemo/Model/StudentListViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace DataBindingDemo.Model
+{
+    public class StudentListViewModel
+    {
+
+        ObservableCollection<Model.StudentViewModel> _studentList;
+
+        public ObservableCollection<Model.StudentViewModel> StudentList
+        {
+            get
+            {
+                _studentList = new ObservableCollection<StudentViewModel>(GetAllStudent());
+                return _studentList;
+            }
+        }
+
+
+
+        #region Repository
+
+        private List<Model.StudentViewModel> GetAllStudent()
+        {
+
+            List<StudentViewModel> studentData = new List<StudentViewModel>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                studentData.Add(new Model.StudentViewModel()
+                {
+                    StudentId = new Random().Next(1000, 9999),
+                    FirstName = $"First Name#{i}",
+                    LastName = $"Last Name#{i}",
+                    Age = new Random().Next(20, 40),
+                    PhoneNo = $"01{i}{new Random().Next(100000, 999999)}",  //Generate random phone number 
+                    TotalLike = new Random().Next(0, 999),
+                    TotalDislike = new Random().Next(0, 999),
+                });
+
+            }
+
+            return studentData;
+        }
+
+        #endregion
+
+    }
+}
---
+++ DataBindingDemo/DataBindingDemo/Model/StudentViewModel.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace DataBindingDemo.Model
+{
+    public class StudentViewModel : INotifyPropertyChanged
+    {
+
+
+        #region Fields
+
+        int _studentId;
+
+        string _firstName;
+
+        string _lastName;
+
+        string _phoneNo;
+
+        DateTime _dateofBirth;
+
+        int _age;
+
+        int _totalLike;
+
+        int _totalDislike;
+
+        #endregion
+
+        #region Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+
+        #region Constructors
+
+        public StudentViewModel()
+        {
+
+        }
+
+
+        #endregion
+
+
+        #region All Properties
+
+        public int StudentId
+        {
+            get
+            {
+                return _studentId;
+            }
+            set
+            {
+                _studentId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string PhoneNo
+        {
+            get
+            {
+                return _phoneNo;
+            }
+            set
+            {
+                _phoneNo = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime DateofBirth
+        {
+            get
+            {
+                return _dateofBirth;
+            }
+            set
+            {
+                _dateofBirth = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                _age = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public int TotalLike
+        {
+            get
+            {
+                return _totalLike;
+            }
+            set
+            {
+                _totalLike = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public int TotalDislike
+        {
+            get
+            {
+                return _totalDislike;
+            }
+            set
+            {
+                _totalDislike = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        #endregion
+
+
+
+        #region Event Handler Functions
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        #endregion
+
+
+
+
+
+    }
+}
