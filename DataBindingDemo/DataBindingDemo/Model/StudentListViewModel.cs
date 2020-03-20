3d12e60671299bbff486fd3c57e7323bc255ed06
KI-IAN <f.r.khan.trk@gmail.com>
2020-03-21 03:00:59 +0800

    ** Command & CommandParameter are added for respective controls and implemented Command codes in view model.

--- DataBindingDemo/DataBindingDemo/Model/StudentListViewModel.cs
+++ DataBindingDemo/DataBindingDemo/Model/StudentListViewModel.cs
@@ -2,22 +2,128 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace DataBindingDemo.Model
 {
     public class StudentListViewModel
     {
 
+        #region Fields
+
         ObservableCollection<Model.StudentViewModel> _studentList;
+
+        #endregion
+
+        #region Properties
 
         public ObservableCollection<Model.StudentViewModel> StudentList
         {
             get
             {
-                _studentList = new ObservableCollection<StudentViewModel>(GetAllStudent());
                 return _studentList;
             }
+
+            set
+            {
+                _studentList = value;
+
+            }
         }
+
+
+        public Command DeleteItemCommand { get; }
+
+        public Command HitLikeCommand { get; }
+
+        public Command HitDislikeCommand { get; }
+
+        #endregion
+
+
+
+        #region Constructor
+
+
+        public StudentListViewModel()
+        {
+
+            #region Initialize Data
+
+            this.StudentList = new ObservableCollection<StudentViewModel>(GetAllStudent());
+
+            #endregion
+
+
+            #region Event Delegation
+
+            DeleteItemCommand = new Command(async (object boundData) => await DeleteItem(boundData));
+
+            HitLikeCommand = new Command(async (object boundData) => await HitLike(boundData));
+
+            HitDislikeCommand = new Command(async (object boundData) => await HitDislike(boundData));
+
+            #endregion
+        }
+
+
+        #endregion
+
+
+        #region Command Handler Functions
+
+
+        public async Task HitDislike(object boundData)
+        {
+            try
+            {
+                var selectedItem = (StudentViewModel)boundData;
+
+                selectedItem.TotalDislike++;
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Operation could not be completed", "Close");
+            }
+        }
+
+
+
+        public async Task HitLike(object boundData)
+        {
+            try
+            {
+                var selectedItem = (StudentViewModel)boundData;
+
+                selectedItem.TotalLike++;
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Operation could not be completed", "Close");
+            }
+        }
+
+
+
+        public async Task DeleteItem(object boundData)
+        {
+            try
+            {
+                var selectedItem = (StudentViewModel)boundData;
+
+                StudentList.Remove(selectedItem);
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Item could not be deleted. Try again later.", "Close");
+
+            }
+        }
+
+
+        #endregion
+
 
 
 
@@ -36,9 +142,9 @@ namespace DataBindingDemo.Model
                     FirstName = $"First Name#{i}",
                     LastName = $"Last Name#{i}",
                     Age = new Random().Next(20, 40),
-                    PhoneNo = $"01{i}{new Random().Next(100000, 999999)}",  //Generate random phone number 
-                    TotalLike = new Random().Next(0, 999),
-                    TotalDislike = new Random().Next(0, 999),
+                    PhoneNo = $"{new Random().Next(010, 019)}{new Random().Next(100000, 999999)}",  //Generate random phone number 
+                    TotalLike = new Random().Next(0, 999),      //Generate Random number of TotalLike
+                    TotalDislike = new Random().Next(0, 999),   //Generate Random number of TotalDislike
                 });
 
             }
