121e56ca19f0a460772d69270ad16d2381747888
KI-IAN <f.r.khan.trk@gmail.com>
2020-04-02 23:12:08 +0800

    ** [CallerMemberName] attribute is used to get caller property name instead of using nameof().

---
+++ DataBindingDemo/DataBindingDemo/Model/PersonViewModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace DataBindingDemo.Model
+{
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+
+        #region Fields
+
+        string _firstName;
+
+        string _lastName;
+
+        string _fullName;
+
+
+
+        #endregion
+
+
+        #region Properties
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged();
+
+            }
+        }
+
+
+        public string FullName
+        {
+            get
+            {
+                return _fullName;
+            }
+            set
+            {
+                _fullName = value;
+                OnPropertyChanged();
+
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+
+        #region Event Handler Function
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        #endregion
+
+
+    }
+}
