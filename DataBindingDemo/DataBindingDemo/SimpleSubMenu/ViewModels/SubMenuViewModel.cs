84cf4684496d0341dba5c2acc3094f036bdb80d0
KI-IAN <f.r.khan.trk@gmail.com>
2020-10-21 23:25:52 +0800

    Main menu event handler is added to simulate opening and closing a menu.

--- DataBindingDemo/DataBindingDemo/SimpleSubMenu/ViewModels/SubMenuViewModel.cs
+++ DataBindingDemo/DataBindingDemo/SimpleSubMenu/ViewModels/SubMenuViewModel.cs
@@ -2,29 +2,98 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace DataBindingDemo.SimpleSubMenu.ViewModels
 {
-    public class SubMenuViewModel
+    public class SubMenuViewModel : INotifyPropertyChanged
     {
 
         private ObservableCollection<GroupedMenuItem> _groupedMenuItems;
+
+
+        #region Implement INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPreopertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
 
         public ObservableCollection<GroupedMenuItem> GroupedMenuItems
         {
             get { return _groupedMenuItems; }
 
-            set { _groupedMenuItems = value; }
+            set
+            {
+                _groupedMenuItems = value;
+                OnPreopertyChanged();
+            }
         }
 
+        public ICommand TapMenu { get; set; }
 
         public SubMenuViewModel()
         {
             SetMenuItems();
 
+            TapMenu = new Command<GroupedMenuItem>((data) => TapMenuHandler(data));
+
         }
 
+
+        private void TapMenuHandler(GroupedMenuItem menu)
+        {
+            //Reload menu
+            SetMenuItems();
+
+            menu.IsExpanded = !menu.IsExpanded;
+
+            // When menu is closed; open the selected menu and close all other menus
+            if (menu.IsExpanded)
+            {
+                var selectedMenu = _groupedMenuItems.Where(r => r.LongName.Equals(menu.LongName, StringComparison.OrdinalIgnoreCase))
+                                    .FirstOrDefault();
+                selectedMenu.StateIcon = "arrow_down.png";
+                selectedMenu.IsExpanded = menu.IsExpanded;
+
+                var unSelectedMenu = _groupedMenuItems.Where(r => !r.LongName.Equals(menu.LongName, StringComparison.OrdinalIgnoreCase));
+
+
+                foreach (var closedMenu in unSelectedMenu)
+                {
+                    closedMenu.StateIcon = "arrow_up.png";
+                    closedMenu.IsExpanded = false;
+
+                    //remove sub menus from those main menus to simulate collapsing
+                    closedMenu.Clear();
+                }
+            }
+            else
+            {
+                foreach (var closedMenus in _groupedMenuItems)
+                {
+                    closedMenus.StateIcon = "arrow_up.png";
+                    closedMenus.IsExpanded = false;
+
+                    //remove sub menus from those main menus to simulate collapsing
+                    closedMenus.Clear();
+                }
+            }
+
+            GroupedMenuItems = _groupedMenuItems;
+
+
+        }
 
         #region Data 
 
